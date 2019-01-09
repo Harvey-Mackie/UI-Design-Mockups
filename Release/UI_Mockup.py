5924885b4f805f5b93078008b08db287ddb4d57c
Harvey Mackie <38426388+hmack001@users.noreply.github.com>
2019-01-09 15:30:24 +0000

    Define Monitor Size
    The size of the monitor function can now be defined, to export a smaller image.

--- Release/UI_Mockup.py
+++ Release/UI_Mockup.py
@@ -82,30 +82,47 @@ def macMockup(img,colour):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def monitorMockup(img,colour):
+def monitorMockup(img,colour,size):
     image = cv2.imread(img, cv2.IMREAD_UNCHANGED)
     
-    image = cv2.resize(image,(821,576))
+    #Image Properties - Declare Variables
+    width = 900
+    height = 900
+    PixelWidthStart=35
+    PixelWidthEnd=863
+    PixelHeightStart = 40
+    PixelHeightEnd = 625
+    MockupImageHeight  =  585
+    MockupImageWidth  =  828
+    ImageProperties = [width, height, PixelWidthStart, PixelWidthEnd, PixelHeightStart,PixelHeightEnd,MockupImageHeight, MockupImageWidth] 
+    if(size=="small"):
+        for i in range(0,len(ImageProperties)):
+            ImageProperties[i] = ImageProperties[i] / 2
+            ImageProperties[i] = int(ImageProperties[i])
+    
+    image = cv2.resize(image,(ImageProperties[7],ImageProperties[6]))
     device = "Monitor"
     #Background Image
     imageLink = "Devices/"+device+"-"+colour+".png"
 
     device_img = cv2.imread(imageLink,  cv2.IMREAD_UNCHANGED)
-    device_img= cv2.resize(device_img,(900,900))
-    #print (image)
-    device_img[45:621, 40:861] = image
+    device_img= cv2.resize(device_img,(ImageProperties[1],ImageProperties[0]))
+
+    device_img[ImageProperties[4]:ImageProperties[5], ImageProperties[2]:ImageProperties[3]] = image
     cv2.imshow("Device", device_img)
 
-    cv2.imwrite("Exports/"+device+"Mockup"+str(random.randint(1,21)*5)+".png",device_img)
+    cv2.imwrite("Exports/"+size+device+"Mockup"+str(random.randint(1,21)*5)+".png",device_img)
     iPadMockup
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 #Alter 'img' and 'deviceColour' to create your own Mockup
-img = "Mockup/Cruyff-Mac.PNG"
+img = "Mockup/Cruyff-Monitor.PNG"
 deviceColour = "White" #Only White Images are imported currentley
-#iPhoneMockup(img,deviceColour)
+size = "small" #Only applies for Monitor 
 iPhoneMockup("Mockup/Cruyff.PNG",deviceColour)
-iPadMockup("Mockup/Cruyff.PNG",deviceColour)
+iPadMockup("Mockup/Cruyff-iPad.PNG",deviceColour)
 macMockup(img,deviceColour)
-monitorMockup(img,deviceColour)
+monitorMockup(img,deviceColour,size)
+monitorMockup("Mockup/Cruyff-Monitor-large.PNG",deviceColour,"large")
+
