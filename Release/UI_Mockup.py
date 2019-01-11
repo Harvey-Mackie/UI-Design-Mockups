9d8bba3ae22526d42240c55455cc8e50502f3903
Harvey Mackie <38426388+hmack001@users.noreply.github.com>
2019-01-11 01:50:31 +0000

    GUI Implementation and Code Re-designed
    The code has been condensed down into one function.
    
    The script now contains a GUI. The GUI is in App.py, and can be executed to view a GUI created from PyQt5 - saves having to manually code and execute functions.
    
    (The old functions remain for testing, the first offical version will not contain these functions.)

--- Release/UI_Mockup.py
+++ Release/UI_Mockup.py
@@ -4,7 +4,6 @@ import random
 import imutils
 
 #Users Image
-
 #def MakeWhiteBlack(device_img):
 def RotateDevice(img,angle):
     img = cv2.imread(img)
@@ -21,21 +20,94 @@ def RotateDevice(img,angle):
     cv2.waitKey(0) # waits until a key is pressed
     cv2.destroyAllWindows() # destroys the window showing image
 
+def CreateMockup(img,colour,size,device):
+    image = cv2.imread(img, cv2.IMREAD_UNCHANGED)
+    #Image Height and Width
+    width = 900
+    height = 900
+    if(device=="iPhone"):
+        PixelWidthStart = 270
+        PixelWidthEnd = 630
+        PixelHeightStart = 143
+        PixelHeightEnd = 757
+        MockupImageHeight = 614
+        MockupImageWidth = 360
+    elif(device=="iPad"):
+        PixelWidthStart = 245
+        PixelWidthEnd = 650
+        PixelHeightStart = 175
+        PixelHeightEnd = 710
+        MockupImageHeight = 535
+        MockupImageWidth = 405
+    elif(device=="Mac"):
+        PixelWidthStart = 193
+        PixelWidthEnd = 707
+        PixelHeightStart = 243
+        PixelHeightEnd = 670
+        MockupImageHeight = 427
+        MockupImageWidth = 514
+    elif(device=="Monitor"):
+        PixelWidthStart=35
+        PixelWidthEnd=863
+        PixelHeightStart = 40
+        PixelHeightEnd = 625
+        MockupImageHeight  =  585
+        MockupImageWidth  =  828
+    ImageProperties = [width, height, PixelWidthStart, PixelWidthEnd, PixelHeightStart,PixelHeightEnd,MockupImageHeight, MockupImageWidth]
+    
+    if(size=="small"):
+        for i in range(0,len(ImageProperties)):
+            ImageProperties[i] = ImageProperties[i] / 2
+            ImageProperties[i] = int(ImageProperties[i])
+        if(device=="Mac"):
+            ImageProperties[5] += -1
+        elif(device=="iPad"):
+            ImageProperties[6] += 1
+            ImageProperties[7] += 1
+            
+    image = cv2.resize(image,(ImageProperties[7],ImageProperties[6]))
+    #Background Image
+    imageLink = "Devices/"+device+"-"+colour+".png"
 
+    device_img = cv2.imread(imageLink,  cv2.IMREAD_UNCHANGED)
+    device_img= cv2.resize(device_img,(ImageProperties[1],ImageProperties[0]))
+
+    device_img[ImageProperties[4]:ImageProperties[5], ImageProperties[2]:ImageProperties[3]] = image
+    cv2.imshow("Device", device_img)
+
+    cv2.imwrite("Exports/"+size+device+"Mockup"+str(random.randint(1,21)*5)+".png",device_img)
+    iPadMockup
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 #Create iPhone Mockup function
-def iPhoneMockup(img,colour):
+def iPhoneMockup(img,colour,size):
     
     image = cv2.imread(img, cv2.IMREAD_UNCHANGED)
     
-    image = cv2.resize(image,(360,614))
+    width = 900
+    height = 900
+    PixelWidthStart = 270
+    PixelWidthEnd = 630
+    PixelHeightStart = 143
+    PixelHeightEnd = 757
+    MockupImageHeight = 614
+    MockupImageWidth = 360
+    ImageProperties = [width, height, PixelWidthStart, PixelWidthEnd, PixelHeightStart,PixelHeightEnd,MockupImageHeight, MockupImageWidth]
+    
+    if(size=="small"):
+        for i in range(0,len(ImageProperties)):
+            ImageProperties[i] = ImageProperties[i] / 2
+            ImageProperties[i] = int(ImageProperties[i])
+    
+    image = cv2.resize(image,(ImageProperties[7],ImageProperties[6]))
     device = "iPhone"
     #Background Image
     imageLink = "Devices/"+device+"-"+colour+".png"
 
     device_img = cv2.imread(imageLink,  cv2.IMREAD_UNCHANGED)
-    device_img= cv2.resize(device_img,(900,900))
+    device_img= cv2.resize(device_img,(ImageProperties[1],ImageProperties[0]))
     
-    device_img[143:757, 270:630] = image
+    device_img[ImageProperties[4]:ImageProperties[5], ImageProperties[2]:ImageProperties[3]] = image
     
     cv2.imshow("Device", device_img)
 
@@ -44,18 +116,36 @@ def iPhoneMockup(img,colour):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def iPadMockup(img,colour):
+def iPadMockup(img,colour,size):
     image = cv2.imread(img, cv2.IMREAD_UNCHANGED)
     
-    image = cv2.resize(image,(405,535))
+    width = 900
+    height = 900
+    PixelWidthStart = 245
+    PixelWidthEnd = 650
+    PixelHeightStart = 175
+    PixelHeightEnd = 710
+    MockupImageHeight = 535
+    MockupImageWidth = 405
+    ImageProperties = [width, height, PixelWidthStart, PixelWidthEnd, PixelHeightStart,PixelHeightEnd,MockupImageHeight, MockupImageWidth]
+    
+    if(size=="small"):
+        for i in range(0,len(ImageProperties)):
+            ImageProperties[i] = ImageProperties[i] / 2
+            ImageProperties[i] = int(ImageProperties[i])
+        ImageProperties[6] += 1
+        ImageProperties[7] += 1
+    
+    
+    image = cv2.resize(image,(ImageProperties[7],ImageProperties[6]))
     device = "iPad"
     #Background Image
     imageLink = "Devices/"+device+"-"+colour+".png"
 
     device_img = cv2.imread(imageLink,  cv2.IMREAD_UNCHANGED)
-    device_img= cv2.resize(device_img,(900,900))
-    #print (image)
-    device_img[175:710, 245:650] = image
+    device_img= cv2.resize(device_img,(ImageProperties[1],ImageProperties[0]))
+
+    device_img[ImageProperties[4]:ImageProperties[5],ImageProperties[2]:ImageProperties[3]] = image
     cv2.imshow("Device", device_img)
 
     cv2.imwrite("Exports/iPadMockup"+str(random.randint(1,21)*5)+".png",device_img)
@@ -63,21 +153,37 @@ def iPadMockup(img,colour):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def macMockup(img,colour):
+def macMockup(img,colour,size):
     image = cv2.imread(img, cv2.IMREAD_UNCHANGED)
     
-    image = cv2.resize(image,(514,427))
+    width = 900
+    height = 900
+    PixelWidthStart = 193
+    PixelWidthEnd = 707
+    PixelHeightStart = 243
+    PixelHeightEnd = 670
+    MockupImageHeight = 427
+    MockupImageWidth = 514
+    ImageProperties = [width, height, PixelWidthStart, PixelWidthEnd, PixelHeightStart,PixelHeightEnd,MockupImageHeight, MockupImageWidth]
+    
+    if(size=="small"):
+        for i in range(0,len(ImageProperties)):
+            ImageProperties[i] = ImageProperties[i] / 2
+            ImageProperties[i] = int(ImageProperties[i])
+        ImageProperties[5] += -1    
+    
+    image = cv2.resize(image,(ImageProperties[7],ImageProperties[6]))
     device = "Mac"
     #Background Image
     imageLink = "Devices/"+device+"-"+colour+".png"
 
     device_img = cv2.imread(imageLink,  cv2.IMREAD_UNCHANGED)
-    device_img= cv2.resize(device_img,(900,900))
+    device_img= cv2.resize(device_img,(ImageProperties[1],ImageProperties[0]))
     #print (image)
-    device_img[243:670, 193:707] = image
+    device_img[ImageProperties[4]:ImageProperties[5], ImageProperties[2]:ImageProperties[3]] = image
     cv2.imshow("Device", device_img)
 
-    cv2.imwrite("Exports/"+device+"Mockup"+str(random.randint(1,21)*5)+".png",device_img)
+    cv2.imwrite("Exports/"+size+device+"Mockup"+str(random.randint(1,21)*5)+".png",device_img)
     iPadMockup
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -117,12 +223,14 @@ def monitorMockup(img,colour,size):
     cv2.destroyAllWindows()
 
 #Alter 'img' and 'deviceColour' to create your own Mockup
-img = "Mockup/Cruyff-Monitor.PNG"
+img = "Mockup/Cruyff-Mac.PNG"
 deviceColour = "White" #Only White Images are imported currentley
-size = "small" #Only applies for Monitor 
-iPhoneMockup("Mockup/Cruyff.PNG",deviceColour)
-iPadMockup("Mockup/Cruyff-iPad.PNG",deviceColour)
-macMockup(img,deviceColour)
-monitorMockup(img,deviceColour,size)
-monitorMockup("Mockup/Cruyff-Monitor-large.PNG",deviceColour,"large")
+size = "smalla" #Only applies for Monitor
+device = "iPhone"
 
+#CreateMockup("Mockup/Cruyff.PNG",deviceColour,size,device)
+#CreateMockup("Mockup/Cruyff-iPad.PNG",deviceColour,size,"iPad")
+#CreateMockup("Mockup/Cruyff-Monitor.PNG",deviceColour,size,"Monitor")
+#CreateMockup(img,deviceColour,size,"Mac")
+
+#iPadMockup("Mockup/Cruyff-Mac.PNG", deviceColour,"small")
